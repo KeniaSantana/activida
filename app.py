3f704cb362e95bf6d04783e77709d8a7ef985dc9
Kenia <23308060610371@cetis61.edu.mx>
2025-10-16 10:47:24 -0500

    x

--- app.py
+++ app.py
@@ -30,5 +30,21 @@ def acerca():
 def formulario():
     return render_template("formulario.html")
 
+
+@app.route("/registro", methods=["POST"])
+def registro():
+    error = None
+    if request.method == "POST":
+        nombre = request.form.get("nombre")
+        apellido = request.form.get("apellido")
+        correo = request.form.get("correo")
+        genero = request.form.get("genero")
+        contraseña=request.form.get("contraseña")
+        dia = request.form.get("dia")
+        mes = request.form.get("mes")
+        año = request.form.get("año")
+    return render_template("formulario.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
